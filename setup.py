8311cdd5c9c9ae6faf51424809a6add2117eff34
user <user@Nokia-N900.(none)>
2010-10-01 20:41:59 +0200

    Fix setup error

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(name='python-sdist-maemo',
       'depends':'python2.5, python-setuptools',
       'XSBC_Bugtracker':'http://khertan.net/sdict_maemo:bugs',
       'XB_Maemo_Display_Name':'Python sdist_maemo',
-      'XB_Maemo_Icon_26':''
+      'XB_Maemo_Icon_26':'',
       'copyright':'gpl'}}
       
       )
